3cd4cee8a27b72e6c0f819ca06f2d87286b53431
K.Y. Dishma Bhashitha Menuranga <bdishma@gmail.com>
2024-07-28 14:54:00 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="flex flex-wrap">
             <div className='w-full lg:w-1/2'>
             <div className="flex flex-col items-center lg:items-start"></div>
-                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Dishma Bhashitha</h1>
+                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Dishma Bhashitha Menuranga</h1>
                 <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Fresh Graduate at SLIIT</span>
             </div>
         </div>
