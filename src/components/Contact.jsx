0c7dcf68b1f6f7e11e655b82d566d191d00f5acb
K.Y. Dishma Bhashitha Menuranga <bdishma@gmail.com>
2024-07-30 14:05:09 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
           <div
             class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-cyan-400 via-cyan-300 to-cyan-400 rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt ">
           </div>
-          <a href="/assets/cv.pdf" download title="Download my CV"
+          <a href="https://drive.google.com/drive/folders/1yScwwG4Lyyp_dW62EGOgXEBaGDJ05RbJ?usp=sharing" download title="Download my CV"
             class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
             role="button">Download my Resume
           </a>
